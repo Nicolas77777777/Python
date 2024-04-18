e8f5b3822ee5cf516bfd8e0165450a76973419d3
Nicolas777777777 <sairwater@gmail.com>
2024-04-18 18:56:40 +0200

    upper , lower, capitalazie method

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -3,8 +3,7 @@
 
 print ("Hello World!")
 
-#
-#
+
 # this variable contains var name
 name: str= "Cesare"
 
@@ -13,6 +12,21 @@ print (f"Ave{name}, etc.......")
 # message 
 message:str=("Moritori tua te salutan")
 
-print(f"{name},{message}")
+
+# use method print(f"{name variabele}"")
+print(f"{name}, {message}")
+
+# upper , lower, capitalazie method
+
+lower_case: str= name.lower()
+upper_case: str= name.upper()
+capitalize_case:str= name.capitalize()
+
+print(f"{lower_case} {upper_case} {capitalize_case}")
+
+
+
+
+
 
 
