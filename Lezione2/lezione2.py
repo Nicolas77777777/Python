59161bae685e74930f384f1d8e2373efd068ea18
Nicolas777777777 <sairwater@gmail.com>
2024-04-18 18:44:05 +0200

    variabile name e f

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -5,7 +5,14 @@ print ("Hello World!")
 
 #
 #
-#
-#
-#
-#
+# this variable contains var name
+name: str= "Cesare"
+
+print (f"Ave{name}, etc.......")
+
+# message 
+message:str=("Moritori tua te salutan")
+
+print(f"{name},{message}")
+
+
