269ee8d2222d1cc8d85da7e7c46acfe9b9352725
Nicolas777777777 <sairwater@gmail.com>
2024-04-19 18:44:42 +0200

    manca una def

---
+++ Lezione2/liste_liste dispari.py
@@ -0,0 +1,19 @@
+def median(l: list[float]) -> float:
+
+
+l:int = [2,9,0,-1,25,2,4,3]
+l = sorted(l)
+mid = len(l) //2
+mediana: float = 0
+if len(l)% 2 == 1: #dispari
+    print("La mia lista ha la lunghezza dispari")
+    mediana= l[mid]
+
+else: # pari
+    print("La mia lunghezza è pari") 
+    mediana = (l[mid]+ l[mid -1]) / 2
+
+return mediana
+
+
+
