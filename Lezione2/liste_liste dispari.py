e9b29f85b5b6d581ee1b78ab3c9fcb6ee2de040e
Nicolas777777777 <sairwater@gmail.com>
2024-04-19 19:27:53 +0200

    prova sbaglaita

--- Lezione2/liste_liste dispari.py
+++ Lezione2/liste_liste dispari.py
@@ -1,23 +1,28 @@
-def median(l: list[float]) -> float:
+# def median(l: list[float]) -> float:
 
 
-l:int = [2,9,0,-1,25,2,4,3]
-l = sorted(l)
-mid = len(l) //2
-mediana: float = 0
-if len(l)% 2 == 1: #dispari
-    print("La mia lista ha la lunghezza dispari")
-    mediana= l[mid]
+# l:int = [2,9,0,-1,25,2,4,3]
+# l = sorted(l)
+# mid = len(l) //2
+# mediana: float = 0
+# if len(l)% 2 == 1: #dispari
+#     print("La mia lista ha la lunghezza dispari")
+#     mediana= l[mid]
 
-else: # pari
-    print("La mia lunghezza è pari") 
-    mediana = (l[mid]+ l[mid -1]) / 2
+# else: # pari
+#     print("La mia lunghezza è pari") 
+#     mediana = (l[mid]+ l[mid -1]) / 2
 
-return mediana
+# return mediana
 
+################################ un altra funzione dove prendo la diffrenza cum
 
-ff
+def diff_cum(l: list [float]) -> float
 
+l:float = [1,2,3,4,5,6]
 
+cont=[]
+for i in len(l):
+    detrazione= i-i 
 
-
+print(detrazione)
