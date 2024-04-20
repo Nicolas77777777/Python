4c747f62322f8d9fb33693972773b66bf0ecb5a4
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-04-21 00:15:54 +0200

    3-9. Dinner Guests:

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -142,11 +142,43 @@ locations: list =["Tokio","Napoli","Valcamonica","Finesterre","Himalaya"]
 print(locations)
 
 #Use sorted() to print your list in alphabetical order without modifying the actual list..
-new_list_locations: list =sorted(locations)
-print(new_list_locations)
-
 print(sorted(locations))
+
+# Show that your list is still in its original order by printing it again.
 print(locations)
+
+#Use sorted() to print your list in reverse-alphabetical order without
+# changing the order of the original list.
+# Show that your list is still in its original order by printing it again.
+print(sorted(locations,reverse=True))
+print(locations)
+
+#Use reverse()  to change the order of your list. 
+#Print the list to show that its order has changed.
+locations.reverse()
+print(locations)
+
+#Use reverse() to change the order of your list again. 
+#Print the list to show it’s back to its original order.
+locations.reverse()
+print(locations)
+
+#Use sort() to change your list so it’s stored in alphabetical order. 
+#Print the list to show that its order has been changed.
+locations.sort()
+print(locations)
+
+#Use sort() to change your list so it’s stored in reverse-alphabetical order.
+#Print the list to show that its order has changed.
+locations.sort(reverse=True)
+print(locations)
+
+#3-9. Dinner Guests:
+
+
+
+
+
 
 
 
