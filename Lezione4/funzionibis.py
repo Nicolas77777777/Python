b721e442498eb1a409615661d8d8ba96a78f0ae2
Nicolas777777777 <sairwater@gmail.com>
2024-04-22 10:07:31 +0200

    funzione bis

--- Lezione4/funzionibis.py
+++ Lezione4/funzionibis.py
@@ -1,2 +1,19 @@
 print("ciao")
 print("Hello")
+
+def subtract_all (x : list[float], y: float) -> list[float]:
+    res: list=[float]= []
+    for elem in x:
+        diff: float = elem-y
+        res.append(diff)
+
+    return res
+
+x=[1,2,3,4,5]
+y= 5
+out= subtract_all(x,y)
+
+print(out)
+
+    #
+    #
