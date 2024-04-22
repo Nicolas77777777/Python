7bca278ace97c86f133d7a0a5a7db2379614355f
Nicolas777777777 <sairwater@gmail.com>
2024-04-22 13:43:50 +0200

    amoroma

---
+++ Lezione4/stringapalindroma.py
@@ -0,0 +1,15 @@
+#def is_palindrome (s: str) -> bool:
+"""Restituisce True se s è palindroma; altrimenti restituisci False
+    e.s "Amo Roma" è una stringa palindroma
+    e.s "Ciao come stai?" non è una stringa palindorma 
+    """
+
+frase1:str = "Amo Roma"
+frase2: str ='Ciao come stai?" non è una stringa palindorma'
+
+frase1: str = frase1.lower().replace('','').replace('\n')
+print(frase1)
+
+
+
+
