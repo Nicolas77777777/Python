1b86076334430ca812714118b136c3871b9105f6
Nicolas777777777 <sairwater@gmail.com>
2024-04-23 13:58:42 +0200

    fibonacci

--- Lezione2/lezione3.py
+++ Lezione2/lezione3.py
@@ -1,5 +1,4 @@
 import time
-
 a: float= time.time()
 
 #4-1. Pizzas
@@ -13,16 +12,47 @@ for i in list_pizza:
 # Modify your for loop to print a sentence using the name of the pizza,
 # instead of printing just the name of the pizza. For each pizza, you should have
 #one line of output containing a simple statement like I like pepperoni pizza.
-my_list: list = []
 
+# create a new list empty for create a new var 
+my_list: list = []
 for i in list_pizza:
     i_like_pizza : str = "Mi piace la pizza " +(i)
-    my_list.append(i_like_pizza + (i))
+    my_list.append(i_like_pizza)
     print(i_like_pizza)
+
 print(my_list)
+
 # Add a line at the end of your program, outside the for loop, that states
 # how much you like pizza. The output should consist of three or more lines 
 # about the kinds of pizza you like and then an additional sentence,
 # such as I really love pizza!
+
+
+piazza_margherita: str = "Con la mozzarella e pomodoro\n oppure con la bufala\n e altre cose"
+pizza_4formaggi: str = " Con tutti i formaggi\n bianca \n e molto croccante"
+
+
+print(piazza_margherita)
+
+
+def fibonacci (numero:int) :
+    if numero <= 2:
+        return 1
+    else:
+        return fibonacci(numero-1)+ fibonacci(numero-2)
+    
+fibonacci(10)
+
+
+def fibonacci_for(numero: int):
+    memoria: list = [1,1]
+    for i in range(1,numero):
+        risultato: int = memoria[i-1]+memoria[i]
+        memoria.append(risultato)
+    return memoria[-1]
+
+print(fibonacci_for(60))
+    
+
 
 
