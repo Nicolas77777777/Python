2da011617fd4e99b556748cf851e7fe1ff34c255
Nicolas777777777 <sairwater@gmail.com>
2024-04-23 11:54:12 +0200

    Lezione3

---
+++ Lezione2/lezione3.py
@@ -0,0 +1,8 @@
+
+#4-1. Pizzas
+
+fun cicli ()
+list_pizza : list =["Margherita","4 Formaggi","Zucca e Guanciale"]
+list_frasi : list =["Mi piace la pizza","Mi piace la pizza","Mi piace la pizza"]
+for i in list_pizza:
+    print(i)
