d34136fcc2d12c6bfbd94037bcf446567b23a525
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-04-23 22:03:48 +0200

    #4-2. Animals

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -16,6 +16,7 @@ my_list: list = []
 for i in list_pizza:
     i_like_pizza : str = "Mi piace la pizza " +(i)
     my_list.append(i_like_pizza)
+
     print(i_like_pizza)
 
 print(my_list)
@@ -24,14 +25,12 @@ print(my_list)
 # how much you like pizza. The output should consist of three or more lines 
 # about the kinds of pizza you like and then an additional sentence,
 # such as I really love pizza!
+print(f"La pizza {list_pizza[0]} è davvero buona con la bufala\nanche la pizza {list_pizza[1]} non scherza\ninfine la pizza {list_pizza[2]} è la mia preferita\nAdoro la pizza.")
+
+#4-2. Animals
 
 
-piazza_margherita: str = "Con la mozzarella e pomodoro\n oppure con la bufala\n e altre cose"
-pizza_4formaggi: str = " Con tutti i formaggi\n bianca \n e molto croccante"
-pizza_Zucca_guanciale: str = "veramente buona con la zucca\n con il guanciale che si scioglie\n e il pecorino"
 
-
-print(piazza_margherita)
 
     
 
