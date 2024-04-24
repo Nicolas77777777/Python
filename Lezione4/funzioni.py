080fe1e32ac4a13374d9e3c02dbfe1ff67e9224e
Nicolas777777777 <sairwater@gmail.com>
2024-04-24 14:04:57 +0200

    prova esame

--- Lezione4/funzioni.py
+++ Lezione4/funzioni.py
@@ -20,43 +20,36 @@
 
 #print(somma_elementi([1,1,1],[1,1,1]))
 	
-# def somma_elementi1(x: list[int], y: list[int]) -> list[int]:
-#     res: list = []
-#     if len(x)  == len(y):
-#         for i in range(len(y)):
-#             r:list=x[i]+y[i]
-#             res.append(r)
-#         return res
-#     elif len(x) != len(y):
-#         z: list = x[0]+y[0]
-#         res.append(z)
-#     return res
-
-# print(somma_elementi1([1,],[1,2]))
-# print(somma_elementi1([1,1,1],[1,1,1]))
-
-
-# def even_odd_pattern(nums: list[int]) -> list[int]:
-#     pari =[x for x in nums if x % 2 ==0]
-#     dispari = [x for x in nums if x % 2 != 0]
-
-#     return dispari, pari 
-
-
-# print(even_odd_pattern([3, 6, 1, 8, 4, 7]))
+def somma_elementi1(x: list[int], y: list[int]) -> list[int]:
+    res: list = []
+    if len(x)  == len(y):
+        for i in range(len(y)):
+            r:list=x[i]+y[i]
+            res.append(r)
+        return res
+    elif len(x) > len(y) :
+        g: list = len
+        print("elif, elif ")
 
 
 
-def even_odd_pattern1(nums: list[int]) -> list[int]:
-    pari=[]
-    dispari=[]
-    for i in nums:
-        if i % 2==0:
-            pari.append(i)
-        elif i % 2 !=0:
-            dispari.append(i)
+
+print(somma_elementi1([1,],[1,2]))
+print(somma_elementi1([1,1,1],[1,1,1]))
+
+
+
+
+# def even_odd_pattern1(nums: list[int]) -> list[int]:
+#     pari=[]
+#     dispari=[]
+#     for i in nums:
+#         if i % 2==0:
+#             pari.append(i)
+#         elif i % 2 !=0:
+#             dispari.append(i)
         
-    return pari + dispari
+#     return pari + dispari
 
-print(even_odd_pattern1([3, 6, 1, 8, 4, 7]))
+# print(even_odd_pattern1([3, 6, 1, 8, 4, 7]))
     
