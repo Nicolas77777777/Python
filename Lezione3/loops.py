b22b9b032442ebe37ddcc4b5d6afea5db49b1ed0
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-04-25 20:03:33 +0200

    prove con if else

---
+++ Lezione3/loops.py
@@ -0,0 +1,81 @@
+import random
+
+l=["c","i","a","o"]
+
+for i in l:
+    print(i)
+
+for j in range(len(l)):
+    print(l[j])
+
+
+i=0
+
+while i < len(l): #-> l=[0,1,2,3]
+    print(l[i])
+    i += 1
+
+## scansione con dict
+
+menu: dict = {"pasta": 10.50, "pizza": 9.00, "salad": 6.50,
+"wine": 4.00, "water": 2.30}
+
+for key in menu:
+    print(f"chiave = {key} valore {menu[key]}")
+
+#trasforma tutte le Key set in una lista/ idem per valori 
+
+keys: list = list(menu.keys())
+values: list = list(menu.values())
+
+print( keys,values)
+
+i=0
+while i < len(keys):
+    print(f"chiave = {keys[i]} {values[i]}")
+    i+=1
+
+# sapendo che la funzione random.randint(start, end)
+# genera un numero intero compreso tra start e end, end compreso,
+# costruire una lista di numeri casuali lunga 100 e
+# stampare la somma di tutti i suoi numeri
+
+lnc1 = []
+lnc = []
+for i in range(100):
+    lnc.append(random.randint(1, 10))
+    lnc1.append(random.randint(1,100))
+
+print(lnc)
+print(lnc1)
+
+
+def prova_if_else (x: float,y: float):
+    l=[]
+    if x > y:  
+        res= x-y
+        l.append(res)
+        return f"{l} questo è se x è maggire di y addiziona"
+    elif x < y:
+        res= x+y
+        l.append(res)
+        return f"{l} questo è se x è minore di y addiziona"
+    elif x==y:
+        res= x**y
+        l.append(res)
+        return f"{l} questo è se x e y sono uguali"
+    else:
+        return f"non è possibile"
+    
+
+out= prova_if_else(-20,50 )
+print(prova_if_else(10004444444,4554253535235325325))
+
+print(out)
+
+
+
+
+
+
+
