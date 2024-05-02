1914cdabf6798ef1df781a2f320457e6d4c0e030
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-05-03 01:09:41 +0200

    8-16

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -237,14 +237,7 @@ def make_car (manufacturer, model, **kwargs ):
 car= make_car(manufacturer="Fiat",model="panda",color="blu", titt=True)
 print(car)
 
-# 8-15. Printing Models: 
-#  Put the functions for the example printing_models.py in a separate file called printing_functions.py.
-#  Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
-
-
-
-
-# 8-16. Imports: 
+#  8-15. Printing Models: / 8-16. Imports: 
 #  Using a program you wrote that has one function in it, store that function in a separate file. 
 #  Import the function into your main program file, and call the function using each of these approaches:
 #  import module_name
