3f75e2a617eff9bd54cd2c919d28ae0d36deb1b7
Nicolas777777777 <sairwater@gmail.com>
2024-05-02 16:54:43 +0200

    esercizi lezione 5

--- Lezione4/lezione5.py
+++ Lezione4/lezione5.py
@@ -7,15 +7,26 @@
 
 def is_magic_number(num: int) -> bool:
   number_str= str(num)
-  for i in range(len(number_str)):
-    if number_str[i] == 7:
+  for i in number_str:
+    if "7" in number_str:
         return True
   else:
     return False
   
-    
 
 print(is_magic_number(70))
+print(is_magic_number(123))
+
+# Scrivi una funzione che somma tutti i numeri interi
+# di una lista che sono maggiori di un dato
+# valore intero definito threshold.
+
+#def sum_above_threshold(numbers: list[int]):
+   
 
 
-#print(is_magic_number(123)
+#print(sum_above_threshold([15, 5, 25], 20))
+
+lista: list =([15, 5, 25], 20)
+for i in lista:
+   print(i)
