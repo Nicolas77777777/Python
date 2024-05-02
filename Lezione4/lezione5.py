51948905e82f684d35e7140f8cbb171d03272823
Nicolas777777777 <sairwater@gmail.com>
2024-05-02 16:28:20 +0200

    esercizi5

--- Lezione4/lezione5.py
+++ Lezione4/lezione5.py
@@ -1,6 +1,21 @@
 # set insieme
 
-firstset: set [int ]={5,4,5,}
-secondstset: set [int ]={5,6,7,}
+# Scrivi una funzione che, dato un numero intero,
+# determina se è un "numero magico".
+# Un numero magico è definito come un 
+# numero che contiene il numero 7.
 
-set.update()
+def is_magic_number(num: int) -> bool:
+  number_str= str(num)
+  for i in range(len(number_str)):
+    if number_str[i] == 7:
+        return True
+  else:
+    return False
+  
+    
+
+print(is_magic_number(70))
+
+
+#print(is_magic_number(123)
