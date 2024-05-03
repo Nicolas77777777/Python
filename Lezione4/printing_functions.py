112145a47b4e2406ff0d14aad12974b489bdd3a6
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-05-03 11:51:36 +0200

    printing functions

--- Lezione4/printing_functions.py
+++ Lezione4/printing_functions.py
@@ -1,8 +1,8 @@
-import lezione4
-from lezione4 import  make_album
-from lezione4 import display_message as dm
-import lezione4 as lz
-from lezione4 import*
+import esercizi_Lezione4_Ob
+from esercizi_Lezione4_Ob import  make_album
+from esercizi_Lezione4_Ob import display_message as dm
+import  esercizi_Lezione4_Ob as lz
+from esercizi_Lezione4_Ob import*
 
 music= make_album("Metallica","Noghing else matter")
 print(music)
