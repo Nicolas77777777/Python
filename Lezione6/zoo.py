13b3b0edee88a32be0235b6d88b67384de68effc
Nicolas777777777 <sairwater@gmail.com>
2024-05-08 12:44:36 +0200

    aggiornamento testo

--- Lezione6/zoo.py
+++ Lezione6/zoo.py
@@ -1,20 +1,27 @@
 
 
+# Creaiamo un sistema di gestione di uno zoo virtuale
+
+# Sistema di gestione dello zoo virtuale
+
+# Classi:
+
 # 1. Zoo: questa classe rappresenta uno zoo. 
-# Lo zoo ha dei recinti e dei guardiani dello zoo.
+# Lo zoo ha dei recinti fences e dei guardiani dello zoo, zoo_keepers.
 
-# 2. Animale: questa classe rappresenta un animale nello zoo.
-# Ogni animale ha questi attributi: nome, specie, età.
+# 2. Animal: questa classe rappresenta un animale nello zoo.
+#   Ogni animale ha questi attributi: name, species, 
+# age, height, width, preferred_habitat, health che è uguale a 100 * (1 / age).
 
-# 3. Recinto: questa classe rappresenta un recinto dello zoo 
-# in cui sono tenuti gli animali. I recinti possono contenere
-# uno o più animali.
-# I recinti possono hanno gli attributi dimensioni,
-# temperatura e tipo di habitat.
+# 3. Fence: questa classe rappresenta un recinto dello zoo in 
+# cui sono tenuti gli animali. I recinti possono contenere uno o 
+# più animali. I recinti possono hanno gli attributi area, temperature e habitat.
 
-# 4. ZooKeeper: questa classe rappresenta un guardiano dello zoo 
-# responsabile della gestione dello zoo. I guardiani dello zoo possono nutrire gli animali,
-# pulire i recinti e svolgere altri compiti.
+# 4. ZooKeeper: questa classe rappresenta un guardiano dello 
+# zoo responsabile della gestione dello zoo. 
+# I guardiani dello zoo hanno un nome, un cognome, e un id. 
+# Essi possono nutrire gli animali, pulire i recinti e
+# svolgere altri compiti nel nostro zoo virtuale.
 
 
 
