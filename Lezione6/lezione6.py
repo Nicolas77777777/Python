fa847d49b9fef6b52585db470e9c280b4f12d8fc
Nicolas777777777 <sairwater@gmail.com>
2024-05-08 10:53:58 +0200

    lezione 6

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -10,11 +10,6 @@ Pandu = Mahābhārata("Pāṇḍu","Pāṇḍava")
 Dhṛtarāṣṭra = Mahābhārata("Dhṛtarāṣṭra","Karuwa")
 
 
-
-
-        
-
-
 # alice = Person("Alice W.", 45)
 # bob = Person("Bob M.", 36)
 
@@ -84,3 +79,41 @@ re.printIn()
 #     #     pass
 
 
+# Exercise 4 (Folder 9 ex_4.py)
+# 1. Write a new class called Food, it should have name, price and
+# description as attributes.
+# 2. Instantiate at least three different foods you know and like.
+# 3. Create a new class called Menu, it should have a list (of Foods) as attribute.
+# __init__ should take a list of Foods as optional parameters (default=[])
+# 4. Create a addFood() and removeFood() for the Menu
+# 5. Create a few new Food instances. Add each to the Menu using the respective
+# Method.
+# 6. Add a method printPrices() that list all items on the Menu with their
+# prices.
+# 7. Add a Menu method getAveragePrice() that returns the average Food
+# price of the Men
+
+
+class Food:
+
+    def _init_  (self, name: str, price: int, descr:str ):
+        self.name = name 
+        self.price = price 
+        self.descr = descr
+
+
+pecorino = Food("Pecorino",3, "formaggio")
+pizza = Food ("pizza",6, "farina e acqua")
+Tonno = Food ("Tonno",14, "pesce")
+
+class Menu:
+
+    def _init_ (self,lista:list[Food]=[]):
+        self.lista= lista
+
+    def addFood(self, AddFood):
+        AddFood
+        
+        
+
+    
