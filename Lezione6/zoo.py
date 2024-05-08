b42a2e4267c749ce3c630c4b36d73de075da462b
Nicolas777777777 <sairwater@gmail.com>
2024-05-08 13:41:40 +0200

    aggiornamnti zoo.py

--- Lezione6/zoo.py
+++ Lezione6/zoo.py
@@ -32,7 +32,7 @@ class Animals:
     def __init__(self, name : str, species : str,
                 age : float, height : float,
                 width : float, preferred_habitat: str,
-                healt: float):
+                ):
 
         self.name: str = name
         self.species: str = species
@@ -40,16 +40,14 @@ class Animals:
         self.height: float = height
         self.width: float = width
         self.preferred_habitat = preferred_habitat
-        self.healt = healt (100 * (1 / age))
-
+        self.health=  100 * (1 / age)
 
 
     def __str__(self) -> str:
         return f'{self.name.capitalize()}(species ={self.species}, age ={self.age} height = {self.height}\
-           width ={self.width} preferred habitat = {self.preferred_habitat} health)'
+           width ={self.width} preferred habitat = {self.preferred_habitat} health ={self.health})'
     
-
-    
+lupo= Animals("Fenfir","lupus",7,30,20,"Foresta")    
 
 class Recinto :
     def __init__(self, dimension, temperature: float, habitat: str):
@@ -62,48 +60,3 @@ class Recinto :
 
 
 class ZooKeeper :
-class Menu:
-    def __init__(self, foods: list[Food] = []):
-        self.foods:list[Food] = foods
-    
-    def addFood(self, food: Food):
-        # food_name in self.foods
-        count: int = 0
-        for food_menu in self.foods:
-            if food.name == food_menu.name:
-                count += 1
-                food_menu.price = food.price
-        if count == 0:
-            self.foods.append(food)
-
-    def removeFood(self, food: Food):
-        if food in self.foods:
-            self.foods.remove(food)
-    
-    def getAvgPrice(self) -> float:
-        total_sum: float = 0
-        for food in self.foods:
-            total_sum += food.price
-        # divido per la lunghezza della lista foods
-        avg_price: float = total_sum/ len(self.foods)
-        return avg_price
-
-    def __str__(self) -> str:
-        repr: str = ""
-        for food in self.foods:
-            repr += food.__str__() + "\n"
-        avg_price: float = self.getAvgPrice()
-        repr += "_" * 30 + "\n"
-        repr += f'Prezzo medio = {avg_price}'
-        return repr
-    
-
-carbonara = Food(name="Carbonara", price=10.5, description="La carbonara è buona")
-cacio_e_pepe = Food(name="cacio_e_pepe", price=15.48)
-un_altra_carbonara = Food(name="Carbonara", price=10.5, description="La carbonara è buona")
-
-menu = Menu()
-menu.addFood(carbonara)
-menu.addFood(cacio_e_pepe)
-menu.addFood(un_altra_carbonara)
-print(menu.__str__())
