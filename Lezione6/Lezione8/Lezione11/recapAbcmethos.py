33e411d0e9c909e96115da1da7e792e4eaa82055
Nio4124 <antoniozottola41204@gmail.com>
2024-05-29 16:22:16 +0200

    recapAbcmethod

---
+++ Lezione6/Lezione8/Lezione11/recapAbcmethos.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+
+
+class AbcAnimal(ABC):
+
+    @abstractmethod
+    def verso(self):
+
+        pass
+
+class Cane(AbcAnimal):
+
+    def __init__(self, nome: str) -> None:
+        self.name: str = nome
+
+    def verso(self):
+        print(f"Bau")
+
+
+
+class Gatto(AbcAnimal):
+
+    def __init__(self, nome: str) -> None:
+        self.name: str = nome
+
+    def verso(self):
+        print(f"MIao")
+
+
+class Coccodrillo(AbcAnimal):
+
+    def __init__(self, nome: str) -> None:
+        self.name: str = nome
+
+    def verso(self):
+        print(f"brrr")
+
+from typing import Any # quando si imposta su typeCheck è utile impostare questa libreria 
+from typing import TypeAlias # rino
+
+tipocomposto : TypeAlias = dict[int, Any]
+
+a: dict[str, Any] = {
+
+    "key_1" : "val_1",
+    "key_2" : "val_2",
+    "key_3" : 3,
+    "key_4" : [1,2]
+
+}
+
+cane_1: Cane = Cane(nome="pluto")
+
+cane_1.verso()
+
+gatto_1: Gatto = Gatto(nome="Silvestro")
+
+gatto_1.verso()
+
+coccodrillo_1: Coccodrillo = Coccodrillo(nome="GIOVANNI")
+
+coccodrillo_1.verso()
+
+
+
+lista_animali: list[AbcAnimal]= [cane_1, gatto_1, coccodrillo_1]
+
+for animale in lista_animali:
+    
+    animale.verso()
+
