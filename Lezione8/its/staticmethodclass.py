d74f6e4524276ca336e9723fc12add2b4fe3f509
Nio4124 <antoniozottola41204@gmail.com>
2024-05-31 15:58:00 +0200

    lezione staticmethod

---
+++ Lezione8/its/staticmethodclass.py
@@ -0,0 +1,22 @@
+"""" Create an abstract class Shape with an abstract method area 
+and another abstract method perimeter. Then, create two subclasses
+ Circle and Rectangle that implement the area and perimeter methods."""
+
+# scrivere la classe 
+
+"""" Exercise 2: Implementing Static Methods
+
+Create a class MathOperations with a static method add 
+that takes two numbers and returns their sum, and another 
+static method multiply  that takes two numbers and returns their product."""
+
+#librery Biblioteca 
+
+
+"""" titolo autore e isbn """
+
+# creare una istanza da una stringa
+
+# utilizzare split sulla virgola titolo autore e isbn 
+
+# 
