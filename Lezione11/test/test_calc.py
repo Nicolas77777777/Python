551c2fa7391399de53fb4e2c1cafd600196ebe67
Nio4124 <antoniozottola41204@gmail.com>
2024-05-31 16:18:45 +0200

    unittest

--- Lezione11/test/test_calc.py
+++ Lezione11/test/test_calc.py
@@ -8,9 +8,7 @@ class TestCalc(unittest.TestCase):
 
         calc_1= Calcutations (a=2, b=3)
         result= calc_1.get_sum()
-
-
         self.assertEqual(result,5,msg=f'Error Test Failded the goal was 5 the funtion returned{result} ')
 
-        if __name__ == "__main__":
-            unittest.main()
+if __name__ == "__main__":
+    unittest.main()
