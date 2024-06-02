e8373ea668d333435752c0c56ef4aaaa52203faf
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-06-03 01:49:00 +0200

    magazzino

--- Lezione11/Esercitazione_sulle_Classi.py
+++ Lezione11/Esercitazione_sulle_Classi.py
@@ -89,8 +89,28 @@ print(film_disponibili)
 
 # Prenotazione dei posti
 print(cinema_1.prenota_film("Il Gladiatore", 30))
-print(cinema_1.prenota_film("Il Gladiatore", 80))  # Questo dovrebbe fallire perché ci sono solo 70 posti disponibili
+print(cinema_1.prenota_film("Il Gladiatore", 80))  
 print(cinema_1.prenota_film("Film Non Disponibile", 30))
+
+
+""" Scrivi un programma in Python che gestisca un magazzino. Il programma deve permettere di aggiungere prodotti al magazzino, cercare prodotti per nome e verificare la disponibilità di un prodotto.
+
+Definisci una classe Prodotto con i seguenti attributi:
+- nome (stringa)
+- quantità (intero)
+ 
+Definisci una classe Magazzino con i seguenti metodi:
+- aggiungi_prodotto(prodotto: Prodotto): aggiunge un prodotto al magazzino.
+- cerca_prodotto(nome: str) -> Prodotto: cerca un prodotto per nome e lo ritorna se esiste.
+- verifica_disponibilità(nome: str) -> str: verifica se un prodotto è disponibile in magazzino.
+ 
+Test case:
+
+    Un gestore del magazzino crea un magazzino e diversi prodotti in diverse quantità. Successivamente, aggiunge i prodotti al magazzino.
+    Il sistema cerca un prodotto per verificare se esiste nell'inventario.
+    Il sistema verifica la disponibilità dei prodotti in inventario."""
+
+
 
 
 
