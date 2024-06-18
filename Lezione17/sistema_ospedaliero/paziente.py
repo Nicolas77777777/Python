14095ab46556f0703ca9f84dc8aaf1974fbda7f0
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-06-19 01:27:51 +0200

    FIRST COMMIT

---
+++ Lezione17/sistema_ospedaliero/paziente.py
@@ -0,0 +1,25 @@
+from persona import Persona
+
+class Paziente(Persona):
+    def __init__(self, first_name: str, last_name: str, idCode: str) -> bool:
+        super().__init__(first_name, last_name)
+
+        self.__idCode = idCode
+
+    def setIdCode(self,idCode) -> None:
+        self.__idCode = idCode
+
+    def getidCode(self) -> str:
+        return self.__idCode
+    
+    def patientInfo(self):
+        print(f'Paziente {self.__first_name} {self.__last_name}')
+        print(f'ID: {self.__idCode}')
+
+s:Paziente= Paziente("Franco","Minkia","11233")
+
+s.patientInfo()
+
+
+
+
