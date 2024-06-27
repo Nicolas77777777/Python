b1079c044330a062ec48ed63c2882d6184ed666e
Nicolas7777777 <sairwater@gmail.com>
2024-06-27 18:59:19 +0200

    thread

--- Lezione20/trehad.py
+++ Lezione20/trehad.py
@@ -2,8 +2,11 @@ def funzione(id: int):
     import time
     import random
 
+
+    sleep_time: int = random.randint(1,10)
+    print(f'{id=}time {time.time}')
     #print(f'{id=} time {time.time()}')
-    time.sleep(1.5)
+    time.sleep(sleep_time)
     print(f'{id=} time {time.time()}')
 
 
@@ -11,23 +14,27 @@ if __name__ == "__main__":
 
     import threading
     import time
+    from concurrent.futures import ThreadPoolExecutor
 
-    list_thread: list[threading.Thread]= []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(funzione, range(10))
 
-    for id in range(3):
+    # list_thread: list[threading.Thread]= []
 
-
-         x: threading.Thread = threading.Thread (target=funzione, args=(1,))
-         list_thread.append(x)
-         print(f" Prima di runnare il thread {time.time()}")
-
-         x.start()
-
-         print(f'ho runnato il thread!{time.time()}')
+    # for id in range(3):
 
 
-    for t in list_thread:
-        t.join()
-        print(f'terminato')
+    #      x: threading.Thread = threading.Thread (target=funzione, args=(1,))
+    #      list_thread.append(x)
+    #      print(f" Prima di runnare il thread {time.time()}")
+
+    #      x.start()
+
+    #      print(f'ho runnato il thread!{time.time()}')
+
+
+    # for t in list_thread:
+    #     t.join()
+    #     print(f'terminato')
 
     
