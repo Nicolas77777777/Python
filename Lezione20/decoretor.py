75577059ae862d8e58b405595c61db3ea5819ea4
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-06-27 18:10:25 +0200

    generetor decorator

---
+++ Lezione20/decoretor.py
@@ -0,0 +1,65 @@
+def say_hello():
+    print(f'Hello Flavio')
+
+def say_ciao () :
+
+    print(f'Mimmo')
+
+# def saluta(func):
+
+#     func('Flavio')
+
+# saluta(say_hello)
+
+
+
+# def parent():
+#     print(f'sono in parent')
+
+#     def first_child():
+
+#         print(f'sono il figlio')
+
+#     def second_child():
+
+#         print(f'Sono in scendo child')
+
+#     return second_child # ritorna il valore in memoriua
+
+# out_function = parent()
+# print(out_function)
+
+# out_function()
+
+import time
+
+def get_time(func):
+    def wrapper(*args):
+        start = time.time()
+
+        func(*args)
+
+        end = time.time()
+        elapsed_time = end - start
+        print(f'{elapsed_time=}')
+
+    return wrapper
+
+
+@get_time
+def say_hello(name: str) -> None:
+    print (f'Hello, {name}')
+
+
+@get_time 
+def random_list():
+    import random
+    import time
+
+    sleep_time: int = random. randint(0,5)
+    time.sleep(sleep_time)
+
+
+
+
+
---
+++ Lezione20/generatore.py
@@ -0,0 +1,21 @@
+def generator():
+
+    yield 1
+    yield 2
+    yield 3
+
+
+prova_genetrator= generator()
+
+print(next(prova_genetrator))
+print(next(prova_genetrator))
+
+print(next(prova_genetrator))
+print(next(prova_genetrator))
+
+print(next(prova_genetrator))
+
+
+
+
+
