c8eb8a5a112f4602ae97711fcb8e31b7ba94b03d
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-07-22 01:44:57 +0200

    fifth commit

---
+++ Lezione10/Esercizi_Condizionali_Cicli_Funzioni.py
@@ -0,0 +1,23 @@
+#QUESTION2
+"""
+Scrivere il frammento di codice che cambi il valore intero memorizzato nella variabile x nel seguente modo:
+- se x è pari, deve essere diviso per 2;
+- se x è dispari deve essere moltiplicato per 3 e gli deve essere sottratto 1.
+
+"""
+
+
+
+#QUESTION3
+
+"""
+Sviluppare una funzione in Python per calcolare lo stipendio 
+lordo di ciascuno dei diversi impiegati. L'azienda paga 10 
+dollari all'ora per le prime 40 ore di lavoro e paga "una volta
+ e mezza" la paga oraria per tutte le ore di lavoro oltre le 40 ore.
+ 
+Per ogni operaio, viene fornito il numero di ore 
+che tale impiegato ha lavorato durante la settimana.
+La vostra funzione deve ricevere questa informazione
+ per ogni impiegato e determinare e stampare lo stipendio lordo.
+"""
