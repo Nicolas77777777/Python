9ef6b5b0d0f12657b34af0a08acd62880c943089
Nicolas7777777 <sairwater@gmail.com>
2024-07-23 18:55:43 +0200

    last commit

--- Lezione27/esercizi._esame.py
+++ Lezione27/esercizi._esame.py
@@ -12,11 +12,11 @@ e ritorni la prima chiave che corrisponde a quel valore,
 """
 
 def trova_chiave_per_valore(dizionario: dict[str: int], valore: int) -> str:
-    for key, value in dict.items():
-        if value == valore :
-            return dict[key]
+    for key, value in dizionario.items():
+        if value == valore:
+            return key
     else:
-        return None
+        return False
     
 print(trova_chiave_per_valore({'a': 100, 'b': 200, 'c': 300}, 200))
 
@@ -40,55 +40,56 @@ print(trova_chiave_per_valore({'a': 100, 'b': 200, 'c': 300}, 200))
             get_balance(account_id): restituisce il saldo del conto con l'ID specificato."""
 
 
-class Account:
+# class Account:
     
-    def __init__(self,account_id: str, balance:float=0 ) -> None:
+#     def __init__(self,account_id: str, balance:float=0 ) -> None:
          
-        self.account_id = account_id
-        self.balance = balance
+#         self.account_id = account_id
+#         self.balance = balance
     
-    def deposit(self,amount: int):
-         self.balance += amount 
+#     def deposit(self,amount: int):
+#          self.balance += amount 
 
-    def get_balance(self):
-         return self.balance
+#     def get_balance(self):
+#          return self.balance
          
 
-class Bank:
+# class Bank:
 
-    def __init__(self, accounts: dict[str, Account],) -> None:
-            self.accounts= accounts
+#     def __init__(self, accounts: dict[str, Account]) -> None:
+#             self.accounts= accounts
 
-    def create_account(self,account_id: str): # crea un nuovo account con l'ID specificato e un saldo pari a 0.
-        self.accounts[account_id] = Account(account_id, 0) 
+#     def create_account(self,account_id: str): # crea un nuovo account con l'ID specificato e un saldo pari a 0.
+#         self.accounts[account_id] = Account(account_id, 0) 
 
-    def deposit(self,account_id, amount: float): # deposita l'importo specificato sul conto con l'ID fornito.
-          if account_id in self.accounts :
-            self.get_balance()
+#     def deposit(self,account_id, amount: float): # deposita l'importo specificato sul conto con l'ID fornito.
+#           if account_id in self.accounts:
+#                pass
+                     
             
-            
-    def get_balance(self,account_id): # restituisce il saldo del conto con l'ID specificato."""
-        if account_id
+                
+#     def get_balance(self,account_id): # restituisce il saldo del conto con l'ID specificato."""
+#         pass
 
-account1= Account("123")
+# account= Account("123")
 
-bank1: Bank = Bank("123",account1)
-
-
-
-
-bank = Bank()
-account1 = bank.create_account("123")
-print(account1.get_balance())
+# #bank1: Bank = Bank("123",account)
 
 
 
-bank = Bank()
-account1 = bank.create_account("123")
-bank.deposit("123",100)
-print(bank.get_balance("123"))
 
-bank = Bank()
-account2 = bank.create_account("456")
-bank.deposit("456",200)
-print(bank.get_balance("456"))
+# bank = Bank()
+# account1 = bank.create_account("123")
+# print(account1.get_balance())
+
+
+
+# bank = Bank()
+# account1 = bank.create_account("123")
+# bank.deposit("123",100)
+# print(bank.get_balance("123"))
+
+# bank = Bank()
+# account2 = bank.create_account("456")
+# bank.deposit("456",200)
+# print(bank.get_balance("456"))
