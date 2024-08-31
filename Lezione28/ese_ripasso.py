d805031f569e07ddcfc077d065adae5ca2df2f53
Nicolas77777777 <140002852+Nicolas77777777@users.noreply.github.com>
2024-08-31 02:05:49 +0200

    first commit

---
+++ Lezione28/ese_ripasso.py
@@ -0,0 +1,31 @@
+#QUESTION 1
+def mappa_parole_a_lunghezza(words: list) -> dict:
+    # cancella pass e scrivi il tuo codice
+    newdict={}
+    for i in words:
+        newdict[i]= len(i)
+    return newdict
+    
+print(mappa_parole_a_lunghezza(["apple", "banana", "cherry"]))
+#QUESTION 2
+
+def trova_tutte_chiavi(dizionario: dict[str: int], valore: int) -> str:
+    """"""
+    pass
+
+#QUESTION 3
+#QUESTION 4 
+#QUESTION 5
+#QUESTION 6
+#QUESTION 7
+#QUESTION 8
+#QUESTION 9
+#QUESTION 10
+#QUESTION 11
+#QUESTION 12
+#QUESTION 13
+#QUESTION 14
+#QUESTION 15
+#QUESTION 16
+#QUESTION 17
+
