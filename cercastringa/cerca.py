72ac82b772c286a43761c718ec97a876eb5bc12b
Nicolas7777777 <sairwater@gmail.com>
2024-09-03 19:00:53 +0200

    thrid commit

--- cercastringa/cerca.py
+++ cercastringa/cerca.py
@@ -1,4 +1,11 @@
 import os
+import PyPDF2
+
+def CercaParolaInFilePdf(sFile,sParola):
+    object = PyPDF2.PdfReader(sFile)
+    numPages = len(object.pages)
+    print (f'il file {sFile} contine{numPages}')
+    return False
 
 def CercaParolaInNomeFile(sFile,sParola):
     sFileLower = sFile.lower()
@@ -16,7 +23,7 @@ def CercaParolaInFileTxt(sPathfile, sParola):
             iRet= sLine.lower().find(sParola.lower())
             if (iRet >=0):
                return True
-            sLine= file1.readlines()
+            sLine= file1.readline()
             print("Riga letta"+ sLine)
     return False
            
@@ -26,8 +33,8 @@ def CercaParolaInContenutoFile(sPathFile, sParola):
     sFileName, sFileExt = os.path.splitext(sPathFile)
     if(sFileExt.lower()==".txt"):
         bRet = CercaParolaInFileTxt(sPathFile, sParola)
-    #if(sFileExt.lower()==".pdf"):
-        #bRet = CercaParolaInFilePdf(sPathFile, sParola)
+    if(sFileExt.lower()==".pdf"):
+        bRet = CercaParolaInFilePdf(sPathFile, sParola)
     return bRet
 
     
@@ -42,19 +49,12 @@ for root, ListDir, ListFiles in os.walk(sRoot):
     print(f'Nella directory {root} ci sono {len(ListDir)} sottodirectory e {len(ListFiles)} file')
     for file in ListFiles:
         print(f'Devo cercare {sParola} in {file}')
-
-
-    bRet = CercaParolaInNomeFile(file,sParola)
-    if (bRet== True):
-        print(f'Trovata parola in file {file}')
-    else:
-        sFilePathCompleto = os.path.join(root,file)
-        bRet = CercaParolaInContenutoFile(sFilePathCompleto,sParola)
-        if (bRet==True):
-            print(f"trovata parola in file {file}")
-
-
-
-CercaParolaInContenutoFile(sRoot,sParola)
-
+        bRet = CercaParolaInNomeFile(file,sParola)
+        if (bRet== True):
+            print(f'Trovata parola in file {file}')
+        else:
+            sFilePathCompleto = os.path.join(root,file)
+            bRet = CercaParolaInContenutoFile(sFilePathCompleto,sParola)
+            if (bRet==True):
+                print(f"trovata parola in file {file}")
 
