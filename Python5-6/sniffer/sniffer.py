1fce783adadd7da49ae5dc1436b375155258b103
Nicolas7777777 <sairwater@gmail.com>
2024-09-13 11:17:52 +0200

    fisrt commit

--- Python5-6/sniffer/sniffer.py
+++ Python5-6/sniffer/sniffer.py
@@ -6,6 +6,9 @@ def process_packet(packet):
 	global iPkt
 	iPkt += 1
 	print("Letto PKT " + str(iPkt))
+
+	with open('pacchetti_sniffati.csv', mode='a', newline='') as file_csv:
+        writer = csv.writer(file_csv)
 	
 	
 sniff(iface="eth0",filter="tcp", prn=process_packet)
