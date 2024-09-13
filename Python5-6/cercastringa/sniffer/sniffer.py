bb1ee819ba71a68db8098bbc87863aa38aa35e97
Nicolas7777777 <sairwater@gmail.com>
2024-09-13 10:04:03 +0200

    sniffer first commit

---
+++ Python5-6/cercastringa/sniffer/sniffer.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+from scapy.layers.http import HTTPRequest 
+from scapy.layers.http import HTTPResponse
+iPkt = 0
+def process_packet(packet):
+	global iPkt
+	iPkt += 1
+	print("Letto PKT " + str(iPkt))
+	
+    
+	
+
+	
+sniff(iface="eth0",filter="tcp", prn=process_packet)
