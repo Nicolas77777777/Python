8ba861912928481f57a9be52c36987603d9e52a8
Nicolas7777777 <sairwater@gmail.com>
2024-09-19 17:47:50 +0200

    second commit

--- Python5-6/progetto/serverweb/server.py
+++ Python5-6/progetto/serverweb/server.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+utenti = [['franco','Rossi','M','0'],
+          ['Nico','Bonnetti','M','0'],
+          ['Gianna','Berti','M','0'],
+          ['Nico','Giraldi']]
+        
 
 api = Flask("__name__")
 
@@ -9,7 +14,13 @@ def index():
 
 @api.route('/registrati', methods=['GET'])
 def form():
-    return render_template('risposta.html')
+    nome = request.args.get('fname')
+    cognome = request.args.get('lname')
+    for utente in utenti:
+        if nome == utente[0] and cognome == utente[1]:
+            return render_template('risposta.html', nome=nome, cognome= cognome)
+    
+    return render_template('ko.html')
 
 
 api.run(host="0.0.0.0", port=8085)
