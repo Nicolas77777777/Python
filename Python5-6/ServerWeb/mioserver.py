1311fee940adb1c9bf62df1ca182051e3cd0363a
Nicolas77777777 <sairwater@gmail.com>
2024-12-04 19:21:47 +0100

    fiveth commit

--- Python5-6/ServerWeb/mioserver.py
+++ Python5-6/ServerWeb/mioserver.py
@@ -9,11 +9,15 @@ def index():
     return render_template('sendfile.html',answer = "ciao")
 
 
-@api.route('/mansendfile',methods=['POST'])
+@api.route('/mansendfile', methods=['POST'])
 def ricevidati():
-    sDomanda= request.form.get("question")
-    mio_answer = " ciao a tutti, ma per ora non risposno alla domanda " + sDomanda
-    return render_template('sendfile.html',answer = mio_answer)
+    sDomanda = request.form.get("question")
+    image = request.files.get("image")
+    if image :
+        image.save("./pippo,jpg")
+    lunghezza = len(image)
+    mio_answer = "ciao a tutti, MA PER ORA NON RISPONDO alla domanda " + sDomanda + "con file " + str(lunghezza)
+    return render_template('send.html',answer=mio_answer)
 
 
 api.run(host="0.0.0", port=8080) 
